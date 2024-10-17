a1347970368e0bf43600109ca107726d133e834e
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-10-17 09:18:05 +0200

    bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Routing from "./components/routing/Routing";
 const App = () => {
