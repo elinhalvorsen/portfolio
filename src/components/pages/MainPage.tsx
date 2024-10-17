7e65edbb9d22e070a7426442aba751e9e1c555f8
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-10-17 09:09:22 +0200

    Prøver å få opp bilde

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -4,7 +4,7 @@ const MainPage = () => {
     <>
       <h1>PORTEFØLJE</h1>
       <p>Her skal det komme prosjekter jeg har holdt på med</p>
-      <img src="/elinhalvorsen.github.io/portfolio/" alt="" />
+      <img src="./portfolio/public/elin-me.png" alt="" />
     </>
   );
 };
