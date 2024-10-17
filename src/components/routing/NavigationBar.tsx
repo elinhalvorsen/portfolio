3374c66c4b37810488c28ca6ce9080c6ba18d620
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-10-17 10:37:59 +0200

    Bilde i navbar

--- src/components/routing/NavigationBar.tsx
+++ src/components/routing/NavigationBar.tsx
@@ -4,8 +4,12 @@ import { Link } from "react-router-dom";
 const NavigationBar = () => {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg">
+      <nav className="navbar navbar-expand-lg" id="navbar-color">
         <div className="container-fluid justify-content-between">
+          <img
+            src="portfolio/public/elin-me.png"
+            alt="Bilde av headsett i navbaren"
+          />
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
