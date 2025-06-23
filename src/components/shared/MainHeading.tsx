84be0aa4b9dee9721f39b801c2f9d62643fb8267
elinhalvorsen <elin_halvorsen99@outlook.com>
2025-06-23 20:52:17 +0200

    Endret på header

--- src/components/shared/MainHeading.tsx
+++ src/components/shared/MainHeading.tsx
@@ -6,7 +6,7 @@ interface IMainHeadning {
 }
 const MainHeading = ({ title, text }: IMainHeadning) => {
   return (
-    <section className="row shadow mt-2 text-white" id="border-heading">
+    <section className="row text-white sticky" id="border-heading">
       <article className="text-center mt-3">
         <h1>{title}</h1>
         <p>{text}</p>
